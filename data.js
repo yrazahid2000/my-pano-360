b026a33b32bae1a97bef1ef630da010c02cc5e03
yrazahid2000 <yrazahid@seznam.cz>
2023-06-04 19:17:17 +0200

    Upload

---
+++ data.js
@@ -0,0 +1,118 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-frdtejnpan3d",
+      "name": "FrýdštejnPan3D",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3927,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.008527982200613238,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "1-frdtejnpan3d12"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-frdtejnpan3d12",
+      "name": "FrýdštejnPan3D12",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 808,
+      "initialViewParameters": {
+        "yaw": 0.0007518552000931322,
+        "pitch": 0,
+        "fov": 1.3843012993600772
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.004101893700685366,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "2-frdtejnpan3d14"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-frdtejnpan3d14",
+      "name": "FrýdštejnPan3D14",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1285,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.3843012993600772
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
